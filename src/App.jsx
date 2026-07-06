4a1d7e5cfc8b1b473fab9c521d872d79f963e078
GennaroBuonomo <gennarobuonomo66@gmail.com>
2026-07-06 17:03:17 +0200

    feat: implement initial layout for weather dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,18 @@ import './App.css'
 
 function App() {
 
-
-  return ()
+  return (
+    <div className="weather-dashboard">
+      <nav className="nav-bar">Navbar</nav>
+      <main className="main-content">
+        <section className="weather-today">Meteo Oggi</section>
+        <section className="forecast">Previsioni 7gg</section>
+        <section className="rain-chart">Grafico Pioggia</section>
+        <section className="global-map">Mappa</section>
+        <aside className="other-cities">Altre città</aside>
+      </main>
+    </div>
+  )
 }
 
 export default App
